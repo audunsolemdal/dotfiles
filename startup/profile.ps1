3d130620c47bac1610d417c46a8cc86eb8e43545
Audun Solemdal <audun@PF1N15A8.bouvet.no>
2020-01-26 12:11:36 +0100

    Remove warning prompt

--- startup/profile.ps1
+++ startup/profile.ps1
@@ -327,24 +327,3 @@ function dirs
         Get-ChildItem -Recurse | Foreach-Object FullName
     }
 }
-
-Import-Module MSTerminalSettings
-$msTermProfileName = 'PowerShell' # Replace with whatever Terminal profile name you're using
-$msTermProfile     = Get-MSTerminalProfile -Name $msTermProfileName
-$script:bombThrown = $false
-function prompt {
-    if ($? -eq $false) {
-        # RED ALERT!!!
-        # Only do this if we're using Microsoft Terminal
-        if ((Get-Process -Id $PID).Parent.Parent.ProcessName -eq 'WindowsTerminal') {
-            Set-MSTerminalProfile -Name $msTermProfile.name -BackgroundImage 'https://media.giphy.com/media/HhTXt43pk1I1W/giphy.gif' -UseAcrylic:$false
-            $script:bombThrown = $true
-        }
-    } else {
-        # Reset to previous settings
-        if ($script:bombThrown) {
-            Set-MSTerminalProfile -Name $msTermProfile.name -BackgroundImage $msTermProfile.backgroundImage -UseAcrylic:$msTermProfile.useAcrylic
-            $script:bombThrown = $false
-        }
-    }
-}
