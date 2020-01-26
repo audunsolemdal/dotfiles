a34c760f4d28162d53b93ebfcd55f389a544d24b
Audun Solemdal <audun@PF1N15A8.bouvet.no>
2020-01-26 12:10:13 +0100

    Workaround for prompt not closing

--- startup/boot.cmd
+++ startup/boot.cmd
@@ -1,13 +1,13 @@
 cd C:\appl\repos\dotfiles
 git pull
 
+copy ".\startup\keybinds.ahk" "%USERPROFILE%\Start Menu\Programs\Startup" /Y
+
 copy ".\startup\profile.ps1" "C:\Windows\System32\WindowsPowerShell\v1.0\Profile.ps1" /Y
 copy ".\startup\profiles.json" "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json" /Y
 
 #PowerShell -Command "Set-ExecutionPolicy RemoteSigned"
 PowerShell -Command "& {c:\appl\repos\dotfiles\startup\start.ps1}"
 
-
-.\startup\keybinds.ahk
 
 exit
