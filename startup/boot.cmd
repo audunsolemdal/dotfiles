a46dcf7672405f51acf24c537c90be3f25cac45d
solomson <matheusrv@email.com>
2020-02-15 17:01:23 +0100

    Update boot.cmd

--- startup/boot.cmd
+++ startup/boot.cmd
@@ -7,7 +7,4 @@ copy ".\startup\windowmanager.ahk" "%USERPROFILE%\Start Menu\Programs\Startup" /
 copy ".\startup\profile.ps1" "C:\Windows\System32\WindowsPowerShell\v1.0\Profile.ps1" /Y
 copy ".\startup\profiles.json" "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json" /Y
 
-#PowerShell -Command "Set-ExecutionPolicy RemoteSigned"
-PowerShell -Command "& {c:\appl\repos\dotfiles\startup\start.ps1}"
-
 exit
