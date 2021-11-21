92f5a77ee8af66e8e3628f7b05c9d369ad42e065
Audun Solemdal <audunlsolemdal@gmail.com>
2021-11-21 17:02:26 +0100

    Formatting

--- main.sh
+++ main.sh
@@ -90,33 +90,28 @@ EOF
         curl -L git.io/antigen > ~/antigen.zsh
 
 cat << EOF >> ~/.zshrc
-        HOME=/mnt/c/appl
+source ~/antigen.zsh
+antigen bundle zsh-users/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-        antigen bundle zsh-users/zsh-autosuggestions
-        source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
 
-        source ~/antigen.zsh
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
 
-        # Load the oh-my-zsh's library.
-        antigen use oh-my-zsh
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
 
-        # Bundles from the default repo (robbyrussell's oh-my-zsh).
-        antigen bundle git
-        antigen bundle heroku
-        antigen bundle pip
-        antigen bundle lein
-        antigen bundle command-not-found
 
-        # Syntax highlighting bundle.
-        antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-autosuggestions
 
-        # Load the theme.
-        antigen theme robbyrussell
-
-        antigen bundle zsh-users/zsh-autosuggestions
-
-        # Tell Antigen that you're done.
-        antigen apply
+# Tell Antigen that you're done.
+antigen apply
 EOF
 
 
